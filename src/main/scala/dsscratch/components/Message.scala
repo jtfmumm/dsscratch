66b85c683be88faf56f114b43a264ad116efd758
John Mumm <jtfmumm@gmail.com>
2015-09-03 16:05:03 -0400

    Clean up

--- src/main/scala/dsscratch/components/Message.scala
+++ src/main/scala/dsscratch/components/Message.scala
@@ -3,5 +3,5 @@ package dsscratch.components
 import dsscratch.clocks.{TS, TimeStamp}
 
 case class Message(cmd: Command, sender: Process, ts: TimeStamp = TS()) {
-  override def toString: String = "Message from " + sender + ": " + cmd
+  override def toString: String = "Message from " + sender + ": " + cmd + " at " + ts
 }
--- src/main/scala/dsscratch/components/Process.scala
+++ src/main/scala/dsscratch/components/Process.scala
@@ -6,21 +6,27 @@ import scala.collection.mutable.ArrayBuffer
 
 trait Process extends Steppable {
   val id: Int
+  var failed = false
+
   val clock: Clock = EmptyClock()
   var log: Log = Log()
+
   val outChs = ArrayBuffer[Channel]()
   val inChs = ArrayBuffer[Channel]()
-  var failed = false
+
   def send(m: Message, ch: Channel): Unit = {
     ch.recv(m)
   }
   def recv(m: Message): Unit
   def fail(): Unit = failed = true
   def restart(): Unit = failed = false
+
+  // For creating a total order
   def >(p: Process) = this.id > p.id
   def >=(p: Process) = this.id >= p.id
   def <(p: Process) = this.id < p.id
   def <=(p: Process) = this.id >= p.id
+
   def addOutChannel(ch: Channel): Unit = {
     if (!outChs.contains(ch)) outChs.append(ch)
   }
@@ -37,13 +43,17 @@ trait Process extends Steppable {
     val i = inChs.indexOf(ch)
     inChs.remove(i)
   }
+
   def takeSnapshot(snapId: TimeStamp): Unit = {}
 }
 
 case class EmptyProcess extends Process {
   val id = 0
+
   def recv(m: Message): Unit = {}
   def step(): Unit = {}
+
+  // For creating a total order
   override def >(p: Process) = false
   override def >=(p: Process) = false
   override def <(p: Process) = true
