851a397ea593d7ef84a729d0e6f32f38e6d9d566
John Mumm <jmumm@theladders.com>
2015-09-08 10:32:28 -0400

    Make TarryRunner default

--- src/main/scala/dsscratch/Run.scala
+++ src/main/scala/dsscratch/Run.scala
@@ -4,9 +4,9 @@ import dsscratch.algos.runners._
 
 object Run {
   def main(args: Array[String]) = {
-//    TarryRunner.runFor(10, 0.5)
+    TarryRunner.runFor(10, 0.5)
 
 //    EchoTesterRunner.runFor(10, 0.5)
-    SimpleBroadcastTesterRunner.runFor(10, 0.5)
+//    SimpleBroadcastTesterRunner.runFor(10, 0.5)
   }
 }
