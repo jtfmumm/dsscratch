64f3897eb55fda108fcfb4b2d298674a4664fcaa
John Mumm <jmumm@theladders.com>
2015-09-10 11:39:53 -0400

    Reduce test density

--- src/main/scala/dsscratch/Run.scala
+++ src/main/scala/dsscratch/Run.scala
@@ -6,8 +6,8 @@ object Run {
   def main(args: Array[String]) = {
 //    TarryRunner.runFor(10, 0.5)
 
-//    EchoTesterRunner.runFor(10, 0.5)
-//    SimpleBroadcastTesterRunner.runFor(10, 0.5)
-    TwoPhaseCommitTesterRunner.runFor(10, 0.5)
+//    EchoTesterRunner.runFor(10, 0.125)
+//    SimpleBroadcastTesterRunner.runFor(10, 0.125)
+    TwoPhaseCommitTesterRunner.runFor(10, 0.125)
   }
 }
